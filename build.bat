9343df32c8c3b4e62a977c4192e263f5ce1efa11
zeramorphic <zeramorphic@proton.me>
2023-08-18 01:30:23 +0100

    Up to near-litter perms
    Signed-off-by: zeramorphic <zeramorphic@proton.me>

---
+++ build.bat
@@ -0,0 +1,2 @@
+@echo off
+latexmk -shell-escape -xelatex -file-line-error -halt-on-error -interaction=nonstopmode -output-directory=build main.tex
---
+++ build.sh
@@ -0,0 +1,1 @@
+latexmk -shell-escape -xelatex -file-line-error -halt-on-error -interaction=nonstopmode -output-directory=build main.tex
